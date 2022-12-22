4e7c8afb2e19474a034233a429aa6868a2eaa771
SofiaRozasNegrao <93043106+SofiaRozasNegrao@users.noreply.github.com>
2022-12-21 23:56:58 -0300

    eu tentei de tudo
    deu problema no git remote add origin e no pull

--- config.js
+++ config.js
@@ -1,9 +1,18 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCcLHxJAdUzVvXtYonMYxDbRwO-sLtpBrw",
+  authDomain: "bancodedados-a8815.firebaseapp.com",
+  databaseURL: "https://bancodedados-a8815-default-rtdb.firebaseio.com",
+  projectId: "bancodedados-a8815",
+  storageBucket: "bancodedados-a8815.appspot.com",
+  messagingSenderId: "602062780669",
+  appId: "1:602062780669:web:e5420a0f1f9eee936aa694"
 };
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
 
